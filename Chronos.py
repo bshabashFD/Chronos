b891b0f9869cf9c8a2f4e5b2c2d6d416232ba6ed
bshabash <bshabash007@outlook.com>
2019-11-22 18:33:22 -0800

    Added some basic files
    Add the first prototype for my program

---
+++ Chronos.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+class Chronos():
+
+    def __init__(self, G = 100, MU=50, LAMBDA=50, p_m=0.1):
+        self.G = 100
+        self.MU = 50
+        self.LAMBDA = 50
+        self.p_m=0.1
+        self.extra_regressors = []
+
+    
+
+    def fit(self, y_df):
+        pass
+    
+    def predict(y_hat_df):
+        pass
